41051ffc045c46be553573df6caf4351a5f9df57
Arturo <artbrare@gmail.com>
2019-06-03 15:00:41 -0500

    cambios de css y estructura

--- app.js
+++ app.js
@@ -5,27 +5,39 @@ var id_password = "loginPassowrd";
 var btn_forgot_password = "btn_forgot_password";
 var btn_iniciar_sesion = "iniciarSesion";
 
+var areas_array = [];
 
 $(document).ready(function() {
+    areas_array = [];
     firebase.auth().onAuthStateChanged(user => {
         if(user) {
+            console.log(user);
             firebase.database().ref(rama_bd_personal + "/" + user.uid + "/areas").once('value').then(function(snapshot){
                 snapshot.forEach(function(childSnapshot){
                     if(childSnapshot.val()){
-                        var area = childSnapshot.key;
-                        console.log(area);
+                        areas_array[areas_array.length] = childSnapshot.key;
                     }
                 });
+                console.log(areas_array);
+                if(areas_array.length > 0){              
+                    window.location.assign(areas_array[0] + ".html");
+                } else {
+                    alert("No tienes área asignada, contacta a un administrador de sistema.")
+                }
             });
 
-        } else {
-        }
+
+        } 
     }); 
 });
 
 $('#' + btn_iniciar_sesion).click(function () { 
+
+
     var userEmail = document.getElementById(id_loginEmail).value;
     var userPass = document.getElementById(id_password).value;
+
+    console.log(userPass);
     
     firebase.auth().signInWithEmailAndPassword(userEmail, userPass).catch(function(error) {
         // Handle Errors here.
@@ -35,14 +47,25 @@ $('#' + btn_iniciar_sesion).click(function () {
         //Abre modal con reset password
         window.alert("Error: " + errorMessage);
     });
-    
+
     firebase.auth().onAuthStateChanged(user => {
         if(user) {
-            // aquí escribir código
-            
-        } else {
-            alert("No se pudo entrar al sistema. Contacta a algún administrador")
-        }
+            firebase.database().ref(rama_bd_personal + "/" + user.uid + "/areas").once('value').then(function(snapshot){
+                snapshot.forEach(function(childSnapshot){
+                    if(childSnapshot.val()){
+                        areas_array[areas_array.length] = childSnapshot.key;
+                    }
+                });
+                console.log(areas_array);
+                if(areas_array.length > 0){               
+                    //window.location.assign(areas_array[0] + ".html");
+                } else {
+                    alert("No tienes área asignada, contacta a un administrador de sistema.")
+                }
+            });
+
+
+        } 
     }); 
 });
 
@@ -53,6 +76,7 @@ $('#' + btn_forgot_password).click(function () {
     
     auth.sendPasswordResetEmail(emailAddress).then(function() {
       // Email sent.
+      alert("Email enviado!");
     }).catch(function(error) {
       // An error happened.
     });
