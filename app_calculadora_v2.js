da8de6fe3ba5ab7b18eb0e920a17556d2e4acdad
Gumer <gumer.rgz@gmail.com>
2019-08-26 21:48:56 -0500

    Html Calculadora y archivo js

---
+++ app_calculadora_v2.js
@@ -0,0 +1,20 @@
+// id's de los elementos HTML
+var id_tab_calculadora = "tabCalculadora";
+var id_form_calculadora = "formCalculadora";
+
+var id_ddl_obraCalculadora = "obraDdlCalculadora";
+var id_ddl_procesoCalculadora = "procesoDdlCalculadora";
+var id_costo_proyectoCalculadora = "costoProyectoCalculadora";
+var id_costo_suministrosCalculadora = "costoSuministrosCalculadora";
+var id_costo_copeoCalculadora = "costoCopeoCalculadora";
+var id_profit_cantidadCalculadora = "profitCantidadCalculadora";
+var id_profit_porcentajeCalculadora = "profitPorcentajeCalculadora";
+var id_precio_ventaCalculadora = "precioVentaCalculadora";
+var id_anticipoCalculadora = "anticipoCalculadora";
+var id_estimacionesCalculadora = "estimacionesCalculadora";
+var id_costo_horasScoreCalculadora = "costoHoraScoreCalculadora";
+var id_indirectosCalculadora = "indirectosCalculadora";
+var id_impuestosCalculadora = "impuestosCalculadora";
+
+var id_agregar_calculadora = "botonGuardarCalculadora";
+var id_borrar_calculadora = "botonBorrarCalculadora";
