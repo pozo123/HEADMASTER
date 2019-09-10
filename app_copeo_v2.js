5381639c95f895a3881391c07a7ba592d671cb71
Gumer <gumer.rgz@gmail.com>
2019-09-10 12:21:03 -0500

    Copeo puestos

--- app_copeo_v2.js
+++ app_copeo_v2.js
@@ -18,6 +18,8 @@ var id_agregar_copeo = "botonAceptarCopeo";
 var id_borrar_copeo = "botonResetCopeo";
 
 var selectTrabajadores;
+var trabajadoresSelectos = [];
+var puestos_json={};
 
 //Variables globales para controlar edición
 
@@ -43,12 +45,15 @@ $('#' + id_tab_copeo).click(function() {
   var puesto;
   var select2 = document.getElementById(id_lista_trabajadoresCopeo);
   firebase.database().ref(rama_bd_datos_referencia + "/puestos").on('value',function(snapshot){
+      puestos_json = snapshot.val();
+      console.log(puestos_json);
       snapshot.forEach(function(snapChild){ //Contar todos los subprocesos de la misma categoría para ese proceso
         puesto = snapChild.val();
         option = document.createElement('option');
         option.value = snapChild.key;
         option.text = puesto.puesto;
         select2.appendChild(option);
+        agregaCamposPuesto(snapChild.key);
       })
       //Llenado de la lista de puestos
       selectTrabajadores = new SlimSelect({
@@ -69,36 +74,64 @@ $('#' + id_borrar_copeo).click(function() {
 });
 
 $('#' + id_lista_trabajadoresCopeo).change(function(){
-  console.log(selectTrabajadores.selected());
-    selectTrabajadores.selected().forEach(function (item){
-      console.log(item);
-      var id_puesto = item+"Copeo";
-      var cantidad = document.createElement('input');
-      cantidad.className = "form-control";
-      cantidad.type = "text";
-      cantidad.placeholder = "Cantidad";
-      cantidad.id = id_puesto;
-
-      var col = document.createElement('div');
-      col.className = "form-group col-6";
-      col.appendChild(cantidad);
-
-      var label = document.createElement('label');
-      label.className = "col-form-label";
-      label.innerHTML = "puesto";
-      label.for = id_puesto;
-
-      var col2 = document.createElement('div');
-      col2.className = "form-group col-6";
-      col2.appendChild(label);
-
-      var row = document.createElement('div');
-      row.className = "form-row";
-      row.id = "row-"+item;
-      row.append(col2);
-      row.append(col);
-
-      var div_trabajadores = document.getElementById(id_div_trabajadoresCopeo);
-      div_trabajadores.appendChild(row);
-    });
+    var array_aux = [];
+    var array_html = selectTrabajadores.selected();
+    var item;
+    for (i = 0; i < array_html.length; i++) {
+      item = array_html[i];
+      if(!trabajadoresSelectos.includes(item)){
+        $('#' + "row-"+item).removeClass('hidden');
+      }
+    }
+    for (i = 0; i < trabajadoresSelectos.length; i++) {
+      item = trabajadoresSelectos.pop();
+      if(!array_html.includes(item)){
+        $('#' + "row-"+item).addClass('hidden');
+      }
+    }
+    trabajadoresSelectos = array_html;
 });
+
+function agregaCamposPuesto(puesto){
+  console.log(puesto);
+  var id_puesto = puesto + "Copeo";
+  var cantidad = document.createElement('input');
+  cantidad.className = "form-group";
+  cantidad.type = "text";
+  cantidad.placeholder = "Cantidad";
+  cantidad.id = id_puesto;
+
+  var col1 = document.createElement('div');
+  col1.className = "col-md-3";
+  col1.appendChild(cantidad);
+
+  var label = document.createElement('label');
+  label.innerHTML = puestos_json[puesto]["puesto"]
+  label.for = id_puesto;
+
+  var col2 = document.createElement('div');
+  col2.className = "col-md-3";
+  col2.appendChild(label);
+
+  var col0= document.createElement('div');
+  col0.className = "col-md-3";
+
+  var col4= document.createElement('div');
+  col4.className = "col-md-3";
+
+  var row = document.createElement('div');
+  row.className = "form-row hidden";
+  row.id = "row-"+puesto;
+  row.append(col4);
+  row.append(col2);
+  row.append(col1);
+  row.append(col0);
+  var div_trabajadores = document.getElementById(id_div_trabajadoresCopeo);
+  div_trabajadores.appendChild(row);
+}
+
+function removeElement(elementId) {
+    // Removes an element from the document
+    var element = document.getElementById(elementId);
+    element.parentNode.removeChild(element);
+}
