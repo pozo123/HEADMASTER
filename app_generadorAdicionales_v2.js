306de5f6e92756108c68db029ad38a65bf8110cd
Gumer <gumer.rgz@gmail.com>
2019-09-18 14:52:04 -0500

    Generador adic html

---
+++ app_generadorAdicionales_v2.js
@@ -0,0 +1,4 @@
+var id_tab_adicionales = "tabGeneradorAdic";
+var id_form_adicionales = "formGeneradorAdic";
+var id_dataTable_buscaAdic = "dataTableBusquedaAdicionales";
+var id_dataTable_selectAdic = "dataTableSeleccionadosAdicionales";
